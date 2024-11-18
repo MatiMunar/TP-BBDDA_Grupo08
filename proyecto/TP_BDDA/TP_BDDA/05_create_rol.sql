ea22e4d98a612457bd23fcffbb74a8b8a4769460
Ivan Nicolas Roig <153671285+IvanRoig@users.noreply.github.com>
2024-11-17 23:07:09 -0300

    chau proyecto

--- proyecto/TP_BDDA/TP_BDDA/05_create_rol.sql
+++
@@ -1,7 +0,0 @@
-USE Com2900G08
-
-IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = 'Supervisor')
-BEGIN
-    CREATE ROLE Supervisor;
-END;
-GO
--- proyecto/TP_BDDA/TP_BDDA/25_exec_abm_ventas.sql
+++
@@ -1,4 +0,0 @@
-USE Com2900G08
-GO
-
-EXEC abm_venta.confirmar_venta @idVenta = 629, @tipoFactura = 'C', @id_pago = 3
